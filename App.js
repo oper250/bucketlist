4e8d77aee9fa1718dec43ebd625de1a1bdf18c24
j_dev <j_developer@naver.com>
2022-06-30 23:37:57 +0900

    first commit1

--- App.js
+++ App.js
@@ -70,7 +70,7 @@ const App: () => Node = () => {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One 1137">
+          <Section title="Step One 137">
             Edit <Text style={styles.highlight}>App.js</Text> to change this
             screen and then come back to see your edits.
           </Section>

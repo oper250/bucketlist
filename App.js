0c058ca44a0ed7c79b90a40a566e59f748867ea4
j_dev <j_developer@naver.com>
2022-06-30 23:42:39 +0900

    first commit1

--- App.js
+++ App.js
@@ -74,7 +74,7 @@ const App: () => Node = () => {
             Edit <Text style={styles.highlight}>App.js</Text> to change this
             screen and then come back to see your edits.
           </Section>
-          <Section title="See Your Changes">
+          <Section title="See You Changes">
             <ReloadInstructions />
           </Section>
           <Section title="Debug">

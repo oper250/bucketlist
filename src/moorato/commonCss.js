78cafc4c6bbac565c8592a19c1303408e0b46932
kimdongwook <projectjk@naver.com>
2022-10-15 20:43:15 +0900

    버킷리스트 수정

--- src/moorato/commonCss.js
+++ src/moorato/commonCss.js
@@ -1,26 +1,60 @@
 import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
-    bigBlue: {
-        color: 'blue',
+    title: {
         fontWeight: 'bold',
-        fontSize: 30,
-    },
-    red: {
-        color: 'red',
-    },
-    yellow: {
-        color: 'yellow',
+        fontSize: 20,
+        color: '#fff',
     },
     mainView: {
         flex: 1,
     },
     topView: {
         flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+        backgroundColor: '#2296f3',
     },
     contentsView: {
-        flex: 9,
-        backgroundColor: 'blue',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 12,
+        backgroundColor: '#ffa500',
+    },
+    bucketItem: {
+        width: 350,
+        height: 120,
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+        backgroundColor: '#ddd',
+        borderRadius: 20,
+        flex: 0,
+        flexDirection: 'row',
+    },
+    bucketImg: {
+        width: 100,
+        height: 100,
+    },
+    bucketTextView: {
+        marginLeft: 20,
+    },
+    bucketTitle: {
+        fontWeight: 'bold',
+        fontSize: 18,
+        color: '#000',
+
+    },
+    bucketDesc: {
+        fontWeight: '400',
+        fontSize: 15,
     }
 })
 
--- src/moorato/main.js
+++ src/moorato/main.js
@@ -1,5 +1,5 @@
 import React, {Component, useEffect, useState} from 'react';
-import {Alert, Button, Platform, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {Alert, Button, Image, Platform, SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import * as sub1 from './sub';
 import * as cu from './commonUtil';
 import styles from './commonCss';
@@ -13,29 +13,47 @@ function TestView1() {
     );
 }
 
-
-
 function MooratoMain() {
     const [number, setNumber] = useState(0);
     useEffect(() => {
         callApi2();
     },[]);
     return (
-        <View style={styles.mainView}>
+        <SafeAreaView style={styles.mainView}>
             <View style={styles.topView}>
                 <Text
-                    style={{backgroundColor: 'red'}}
+                    style={styles.title}
                 >버킷리스트
                 </Text>
             </View>
-            <View style={styles.contentsView}></View>
-        </View>
+            <View style={styles.contentsView}>
+                <View style={styles.bucketItem}>
+                    <Image
+                        style={styles.bucketImg}
+                        source={{uri: 'https://img.j-devs.net/study-app/bucket/rep/202210/mUKF1wF420221013S04vSo5x.jpg'}}
+                    />
+                    <View style={styles.bucketTextView}>
+                        <Text style={styles.bucketTitle}>숲속에서 30일살기</Text>
+                        <Text style={styles.bucketDesc}>숲속에서 30일살기</Text>
+                    </View>
+                </View>
+                <View style={styles.bucketItem}>
+                    <Image
+                        style={styles.bucketImg}
+                        source={{uri: 'https://img.j-devs.net/study-app/bucket/rep/202210/mUKF1wF420221013S04vSo5x.jpg'}}
+                    />
+                    <View style={styles.bucketTextView}>
+                        <Text style={styles.bucketTitle}>숲속에서 30일살기</Text>
+                        <Text style={styles.bucketDesc}>숲속에서 30일살기</Text>
+                    </View>
+                </View>
+            </View>
+        </SafeAreaView>
     );
 }
 
 // API 호출
 function callApi2() {
-
     const param = {
         svcGb: 'B',
         userId: 'oper250',

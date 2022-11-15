9a0e2a9f553875320a80cdd866bd812e3e0a7b27
j_dev <j_developer@naver.com>
2022-11-15 21:48:08 +0900

    commit

--- src/jdevs/main.js
+++ src/jdevs/main.js
@@ -1,11 +1,51 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {SafeAreaView, Text, View} from 'react-native';
+import {loginStyle} from "./css.js";
 
 function JDevsMain() {
     return (
-      <View>
-          <Text>주정식 홈입니다.</Text>
-      </View>
+        <SafeAreaView style={loginStyle.safeAreaView}>
+            {/* blank view */}
+            <View style={loginStyle.blankFlexView30percent}></View>
+
+            {/* login-form */}
+            <View style={loginStyle.loginFormView}>
+
+                {/* login-title */}
+                <View style={[loginStyle.loginTtlView,loginStyle.blankFlexView20percent]}>
+                    <Text style={loginStyle.loginTtlText}>로그인</Text>
+                </View>
+
+                {/* login-input */}
+                <View style={[loginStyle.loginInputView,loginStyle.blankFlexView60percent]}>
+
+                    {/* login-input tr 1 : 아이디 */}
+                    <View style={[loginStyle.loginInputTrView,loginStyle.blankFlexView30percent]}>
+
+                        {/* login-input th */}
+                        <View style={[loginStyle.loginInputThView,loginStyle.blankFlexView30percent]}>
+                            <Text style={loginStyle.loginThText}>아이디 :</Text>
+                        </View>
+                        <View style={[loginStyle.loginInputTdView,loginStyle.blankFlexView70percent]}>
+                            <Text style={loginStyle.loginTdText}>비밀번호</Text>
+                        </View>
+                    </View>
+
+                    {/* login-input tr 2 : 비밀번호 */}
+                    <View style={loginStyle.blankFlexView50percent}>
+                        <Text style={loginStyle.loginTtlText}>버튼</Text>
+                    </View>
+                </View>
+
+                {/* login-button */}
+                <View style={[loginStyle.loginBtnView,loginStyle.blankFlexView20percent]}>
+
+                </View>
+            </View>
+
+            {/* blank view */}
+            <View style={loginStyle.blankFlexView30percent}></View>
+        </SafeAreaView>
     );
 }
 
